fd0494f1e2aaad7d000ecbf0d37d7f3eb9bcb25f
nicolasjoaquinparisi <nicojp.1808@gmail.com>
2025-05-04 17:09:43 -0300

    Add initial project files including .gitignore, package.json, and job processing logic

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import semaphore from "semaphore";
+import { readFile } from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import Queue from "./queue.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const data = await readFile(join(__dirname, "../data/jobs.json"), "utf-8");
+const jobs = JSON.parse(data);
+
+const sem = semaphore(2);
+
+const jobsQueue = new Queue();
+
+jobs.forEach((job) => {
+  jobsQueue.enqueue({
+    id: job.id,
+    secondsToComplete: job.secondsToComplete,
+  });
+});
+
+function processJob(job) {
+  return new Promise((resolve, reject) => {
+    const jobWillFail = Math.random() < 0.5;
+
+    if (jobWillFail) {
+      return reject(new Error(`Job with id ${job.id} failed`));
+    }
+
+    setTimeout(() => {
+      resolve(`✅ Job with id ${job.id} completed`);
+    }, job.secondsToComplete * 1000);
+  });
+}
+
+function processQueue() {
+  if (jobsQueue.isEmpty()) {
+    console.log("All jobs processed");
+    return;
+  }
+
+  const job = jobsQueue.dequeue();
+
+  sem.take(() => {
+    processJob(job)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(`❌ ${error.message}`);
+      })
+      .finally(() => {
+        sem.leave();
+        processQueue();
+      });
+  });
+}
+
+console.log("Starting job processing...");
+processQueue();
